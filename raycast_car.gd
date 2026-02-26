7d17f9617aebe3fb067d6b8a600085fe60e8a306
Pepe <pepe2708yt@gmail.com>
2026-02-26 03:58:05 +0000

    Switched to shapecasts for wheels

--- raycast_car.gd
+++ raycast_car.gd
@@ -10,8 +10,9 @@ extends RigidBody3D
 @export var max_turn_curve : Curve
 
 @export_group("Wheel properties")
+@export var offset_shapecast : float = 0.3
 @export var spring_strength := 5000.0
-@export var spring_damping := 200.0
+@export var spring_damping := 100.0
 @export var rest_dist := 0.1
 @export var over_extend := 0.05
 @export var wheel_radius := 0.27
@@ -25,7 +26,7 @@ extends RigidBody3D
 var car_speed_ratio = 0.0
 
 func _get_point_velocity(point: Vector3) -> Vector3:
-	return linear_velocity + angular_velocity.cross(point - global_position)
+	return linear_velocity + angular_velocity.cross(point - to_global(center_of_mass))
 
 func _physics_process(delta: float) -> void:
 	var throttle_input := Input.get_action_strength("throttle")
@@ -36,10 +37,13 @@ func _physics_process(delta: float) -> void:
 
 	for wheel in wheels:
 		var wheel_mesh: Node3D = wheel.get_child(0)
+		var shapecast: ShapeCast3D = wheel.get_child(1)
 		var wheel_center := wheel_mesh.global_position
 		var force_pos := wheel_center - global_position
 		wheel.target_position.y = -(rest_dist + wheel_radius + over_extend)
-		wheel.force_raycast_update()
+		shapecast.target_position.x = -(rest_dist + over_extend) - offset_shapecast
+		shapecast.add_exception(wheel.get_parent())
+		shapecast.position.y = offset_shapecast
 
 		## Rotate wheels
 		var is_steering_wheel := to_local(wheel.global_position).z < 0
@@ -59,20 +63,20 @@ func _physics_process(delta: float) -> void:
 		var wheel_forward_velocity := wheel_forward_dir.dot(linear_velocity)
 		wheel_mesh.rotate_x((-wheel_forward_velocity * delta) / wheel_radius)
 		
-		if not wheel.is_colliding(): continue
+		if not wheel.is_colliding(): continue # TODO: Remove
+		if not shapecast.is_colliding(): continue
 		
-		var contact_point := wheel.get_collision_point()
+		var contact_point := wheel.get_collision_point() # TODO: Remove
+		contact_point = shapecast.get_collision_point(0)
 		var spring_len := maxf(0.0, wheel.global_position.distance_to(contact_point) - wheel_radius)
 		var spring_offset := rest_dist - spring_len
-		
-		# Smooth wheel movement over bumps. TODO: Consider using shapecast instead
-		wheel_mesh.position.y = move_toward(wheel_mesh.position.y, -spring_len, 5 * delta)
+		wheel_mesh.position.y = -spring_len
 		
 		## Suspension
 		var spring_force := spring_strength * spring_offset
 		var tire_velocity := _get_point_velocity(wheel_center)
 		var damping_force := spring_damping * wheel.global_basis.y.dot(tire_velocity)
-		var suspension_force = (spring_force - damping_force) * wheel.get_collision_normal()
+		var suspension_force = (spring_force - damping_force) * shapecast.get_collision_normal(0)
 		apply_force(suspension_force, force_pos)
 		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, suspension_force, 0.02, Color.BLUE, 0.3, true)
 		
@@ -87,14 +91,15 @@ func _physics_process(delta: float) -> void:
 		## Grippy steering
 		var wheel_sideways_dir := wheel.global_basis.x
 		var wheel_sideways_velocity := wheel_sideways_dir.dot(tire_velocity)
-		var grip_impulse := -wheel_sideways_velocity * car_mass_share * wheel_sideways_dir
-		apply_impulse(grip_impulse, force_pos)
-		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, grip_impulse, 0.5, Color.YELLOW, 0.3, true)
+		var grip_factor := 0.6 
+		var grip_force := (-wheel_sideways_velocity * car_mass_share / delta) * grip_factor * wheel_sideways_dir
+		apply_force(grip_force, force_pos)
+		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, grip_force, 0.5, Color.YELLOW, 0.3, true)
 		
 		## Rolling resistance
 		var rolling_resistance := rolling_resistance_coef
 		if brake_input > 0.0:
 			rolling_resistance += brake_power * brake_input
-		var rolling_resistance_impulse := wheel.global_basis.z * wheel_forward_velocity * rolling_resistance * car_mass_share
-		apply_impulse(rolling_resistance_impulse, force_pos)
-		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, rolling_resistance_impulse, 1.0, Color.ORANGE, 0.3, true)
+		var rolling_resistance_force := wheel.global_basis.z * wheel_forward_velocity * (rolling_resistance * car_mass_share / delta)
+		apply_force(rolling_resistance_force, force_pos)
+		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, rolling_resistance_force, 1.0, Color.ORANGE, 0.3, true)
