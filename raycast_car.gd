f99700deba1b7fa2c587725afff5bac66920c0b2
Pepe <pepe2708yt@gmail.com>
2026-02-26 07:54:31 +0000

    Removed erronious wheel radius addition

--- raycast_car.gd
+++ raycast_car.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
 @export_group("Car properties")
-@export var wheels: Array[RayCast3D]
+@export var wheels: Array[ShapeCast3D]
 @export var acceleration := 600.0
 @export var max_speed := 20.0
 @export var accel_curve : Curve
@@ -10,9 +10,8 @@ extends RigidBody3D
 @export var max_turn_curve : Curve
 
 @export_group("Wheel properties")
-@export var offset_shapecast : float = 0.3
 @export var spring_strength := 5000.0
-@export var spring_damping := 100.0
+@export var spring_damping := 200.0
 @export var rest_dist := 0.1
 @export var over_extend := 0.05
 @export var wheel_radius := 0.27
@@ -37,13 +36,9 @@ func _physics_process(delta: float) -> void:
 
 	for wheel in wheels:
 		var wheel_mesh: Node3D = wheel.get_child(0)
-		var shapecast: ShapeCast3D = wheel.get_child(1)
 		var wheel_center := wheel_mesh.global_position
 		var force_pos := wheel_center - global_position
-		wheel.target_position.y = -(rest_dist + wheel_radius + over_extend)
-		shapecast.target_position.x = -(rest_dist + over_extend) - offset_shapecast
-		shapecast.add_exception(wheel.get_parent())
-		shapecast.position.y = offset_shapecast
+		wheel.target_position.y = -(rest_dist + over_extend)
 
 		## Rotate wheels
 		var is_steering_wheel := to_local(wheel.global_position).z < 0
@@ -63,20 +58,17 @@ func _physics_process(delta: float) -> void:
 		var wheel_forward_velocity := wheel_forward_dir.dot(linear_velocity)
 		wheel_mesh.rotate_x((-wheel_forward_velocity * delta) / wheel_radius)
 		
-		if not wheel.is_colliding(): continue # TODO: Remove
-		if not shapecast.is_colliding(): continue
+		if not wheel.is_colliding(): continue
 		
-		var contact_point := wheel.get_collision_point() # TODO: Remove
-		contact_point = shapecast.get_collision_point(0)
+		var contact_point := wheel.get_collision_point(0)
 		var spring_len := maxf(0.0, wheel.global_position.distance_to(contact_point) - wheel_radius)
 		var spring_offset := rest_dist - spring_len
-		wheel_mesh.position.y = -spring_len
 		
 		## Suspension
 		var spring_force := spring_strength * spring_offset
 		var tire_velocity := _get_point_velocity(wheel_center)
 		var damping_force := spring_damping * wheel.global_basis.y.dot(tire_velocity)
-		var suspension_force = (spring_force - damping_force) * shapecast.get_collision_normal(0)
+		var suspension_force = (spring_force - damping_force) * wheel.get_collision_normal(0)
 		apply_force(suspension_force, force_pos)
 		if show_debug: DebugDraw3D.draw_arrow_ray(wheel_center, suspension_force, 0.02, Color.BLUE, 0.3, true)
 		
