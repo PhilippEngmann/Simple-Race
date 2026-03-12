fa041ddbe9d916a17e9596ea2f4885ac3835114f
Pepe <pepe2708yt@gmail.com>
2026-03-12 03:03:48 +0000

    Added improved D14

--- raycast_car.gd
+++ raycast_car.gd
@@ -10,19 +10,21 @@ extends RigidBody3D
 @export_group("Wheel properties")
 @export var spring_strength := 5000.0
 @export var spring_damping := 600.0
-@export var rest_dist := 0.1
+@export var rest_dist := 0.2
 @export var over_extend := 0.0
 @export var wheel_radius := 0.3
 @export var rolling_resistance_coef := 0.01
 @export var brake_power := 0.005
 @export var grip_curve_front : Curve
 @export var grip_curve_rear : Curve
-@export var pacejka : Curve
+@export var grip_curve_drift_front : Curve
+@export var grip_curve_drift_rear : Curve
 
 @export_category("Debug")
 @export var show_debug := false
 
 @onready var total_wheels := wheels.size()
+var is_drifting := false
 
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - to_global(center_of_mass))
@@ -67,7 +69,6 @@ func _physics_process(delta: float) -> void:
 		var spring_force := spring_strength * spring_offset
 		var damping_force := spring_damping * wheel.global_basis.y.dot(tire_velocity)
 		var suspension_force = (spring_force - damping_force) * wheel.get_collision_normal(0)
-		#print
 		apply_force(suspension_force, force_pos)
 		if show_debug: DebugDraw3D.draw_arrow_ray(global_position + force_pos, suspension_force, 0.02, Color.BLUE, 0.3, true)
 		
@@ -82,14 +83,22 @@ func _physics_process(delta: float) -> void:
 		var wheel_sideways_dir := wheel.global_basis.x
 		var wheel_sideways_velocity := wheel_sideways_dir.dot(tire_velocity)
 		
-		var slip_angle = atan2(wheel_sideways_velocity, max(abs(wheel_forward_velocity), 1.0))
+		var slip_angle = atan2(wheel_sideways_velocity, wheel_forward_velocity)
+		var slip_angle_norm = remap(abs(slip_angle), 0, PI/2, 0, 1)
 		
-		#var grip_factor := grip_curve_front.sample_baked(absf(wheel_sideways_velocity/tire_velocity.length()))
+		if brake_input > 0: is_drifting = true
+		if brake_input == 0 and slip_angle_norm < 0.05: is_drifting = false
+		
+		var grip_factor := grip_curve_front.sample_baked(slip_angle_norm)
+		if !is_front_wheel: grip_factor = grip_curve_rear.sample_baked(slip_angle_norm)
+		if is_drifting:
+			grip_factor = grip_curve_drift_front.sample_baked(slip_angle_norm)
+			if !is_front_wheel: grip_factor = grip_curve_drift_rear.sample_baked(slip_angle_norm)
 		var normal_load := car_mass_share * 9.8
-		var friction_coef := 1.0
-		if is_front_wheel: friction_coef = 1.0
-		var grip_force := -wheel_sideways_velocity * wheel_sideways_dir * friction_coef * normal_load# * pacejka.sample_baked(deg_to_rad(slip_angle))
+		var grip_force := -wheel_sideways_velocity * wheel_sideways_dir * normal_load * grip_factor
+		print(rad_to_deg(abs(slip_angle)))
 		apply_force(grip_force, force_pos)
+		
 		if show_debug: DebugDraw3D.draw_arrow_ray(global_position + force_pos, grip_force, 0.5, Color.YELLOW, 0.3, true)
 		
 		## Rolling resistance
